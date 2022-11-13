b192140d29b37a121e98fa4054f3943547617784
Luca Hilbrich <60258241+luuuucaaa@users.noreply.github.com>
2022-11-13 18:25:51 +0100

    Add files via upload

--- tasks.js
+++ tasks.js
@@ -1,33 +1,53 @@
-const TASKS = [
-    'bank robbery',
-    'car crash',
-    'rain storm',
-    'duck hunting',
-    'football game',
-    'university lecture',
-    'earthquake',
-    'traffic jam',
-    'funeral',
-    'rollercoaster ride',
-    'moon landing',
-    'yoga session',
-    'auction',
-    'talk show',
-    'radio show',
-    'demolition of building'
+TASKS = [
+    [
+        'Attending a grill party with some friends in a park.',
+        'Celebrating New Years with friends but fireworks are banned.',
+        'Camping with friends, cooking food over a bonfire.'
+    ],
+    [
+        'Throwing a water bomb during a children\'s birthday party.',
+        'Accidentally dropping a glass of water during your lunch break.',
+        'Flipping a stone over the water at a public lake.'
+    ],
+    [
+        'Jumping from a dive tower into a swimming pool.',
+        'Jumping from a tree into a lake.',
+        'Jumping from a pedestrian bridge into a river.'
+    ],
+    [
+        'Deflating a tire after pulling the plug.',
+        'Opening a can of beer after removing it from the sixpack.',
+        'Airlock of a spaceship opens for an alien creature.'
+    ],
+    [
+        'Talking about politics with a friend.',
+        'Argumenting about the dinner place with the partner.',
+        'Ecstatic conversation of a football match with a stranger.'
+    ],
+    [
+        'Car crash on a crossing.',
+        'Bicycle crash on a busy crossing.',
+        'Train crash at a train station.'
+    ],
+    [
+        'Dropping a pot while staying at your appartement.',
+        'Looking out of your window, seeing metal tubes fall down on a construction site.',
+        'Witnessing a meteorite impact while sitting on your balcony.'
+    ],
+    [
+        'Someone hits the center spot on a shooting range.',
+        'Witnessing a bank robbery with shootings.',
+        'Tiger Woods hits a birdie.'
+    ],
+    [
+        'Firefighters extinguishing a house fire.',
+        'Firefighters extinguishing a forest fire.',
+        'Extinguishing a kebab pike with a home extinguisher.'
+    ]
 ]
 
 function getRandomTasks()
 {
-    let randomPicks = [];
-    while(randomPicks.length < 3) {
-        let r = Math.floor(Math.random() * TASKS.length);
-        if(randomPicks.indexOf(r) === -1) randomPicks.push(r);
-    }
-    let randomTasks = [
-        TASKS[randomPicks[0]],
-        TASKS[randomPicks[1]],
-        TASKS[randomPicks[2]]
-    ];
-    return randomTasks;
+    let r = Math.floor(Math.random() * TASKS.length);
+    return [TASKS[r][0], TASKS[r][1], TASKS[r][2]]
 }
